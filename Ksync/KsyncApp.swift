aa80d7e50a8acf27bf08acab9d1e064a6a9c4d2b
Vishek Tyagi <tyagivishek1234@gmail.com>
2026-03-24 18:35:33 +0530

    Initial Commit

---
+++ Ksync/KsyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  KsyncApp.swift
+//  Ksync
+//
+//  Created by vishek on 24/03/26.
+//
+
+import SwiftUI
+
+@main
+struct KsyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KsyncTests/KsyncTests.swift
@@ -0,0 +1,17 @@
+//
+//  KsyncTests.swift
+//  KsyncTests
+//
+//  Created by vishek on 24/03/26.
+//
+
+import Testing
+@testable import Ksync
+
+struct KsyncTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
